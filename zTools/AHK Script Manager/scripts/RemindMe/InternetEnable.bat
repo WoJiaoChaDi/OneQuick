0582829833a53d3eca74ba61cfd159a8b0110630
WoJiaoChaDi <264835229@qq.com>
2021-04-09 13:59:46 +0800

    bat批处理

--- zTools/AHK Script Manager/scripts/RemindMe/InternetEnable.bat
+++ zTools/AHK Script Manager/scripts/RemindMe/InternetEnable.bat
@@ -5,4 +5,4 @@ netsh interface set interface "以太网" disable
 netsh interface show interface "以太网 3"
 
 
-netsh interface show interface|findstr "以太网 3"|findstr "已启用" && (netsh interface set interface "以太网 3" disable)
+netsh interface show interface|findstr "以太网 3"|findstr "已启用" && netsh interface set interface "以太网 3" disable
